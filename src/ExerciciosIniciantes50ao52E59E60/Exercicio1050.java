5e63387c0d123fcc480328d3ed9750e7e2caf32e
Leonardo Ferreira <leonardoferreiraprogramador@gmail.com>
2023-04-10 20:57:33 -0300

    exercicios 50 e 51

---
+++ src/ExerciciosIniciantes50ao52E59E60/Exercicio1050.java
@@ -0,0 +1,61 @@
+package ExerciciosIniciantes50ao52E59E60;
+
+import java.io.IOException;
+import java.util.Scanner;
+/*
+Leia um número inteiro que representa um código de DDD para discagem interurbana. Em seguida, informe à qual cidade o
+DDD pertence, considerando a tabela abaixo:
+
+Se a entrada for qualquer outro DDD que não esteja presente na tabela acima, o programa deverá informar:
+DDD nao cadastrado
+
+Entrada
+A entrada consiste de um único valor inteiro.
+
+Saída
+Imprima o nome da cidade correspondente ao DDD existente na entrada. Imprima DDD nao cadastrado caso não existir DDD
+correspondente ao número digitado.
+ */
+
+public class Exercicio1050 {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        int ddd = sc.nextInt();
+
+        switch (ddd) {
+            case 61:
+                System.out.println("Brasilia");
+                break;
+            case 71:
+                System.out.println("Salvador");
+                break;
+            case 11:
+                System.out.println("Sao Paulo");
+                break;
+            case 21:
+                System.out.println("Rio de Janeiro");
+                break;
+            case 32:
+                System.out.println("Juiz de Fora");
+                break;
+            case 19:
+                System.out.println("Campinas");
+                break;
+            case 27:
+                System.out.println("Vitoria");
+                break;
+            case 31:
+                System.out.println("Belo Horizonte");
+                break;
+            default:
+                System.out.println("DDD nao cadastrado");
+        }
+
+        sc.close();
+    }
+
+
+}
